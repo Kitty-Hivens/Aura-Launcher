67da685545ed9972da0c41134fd4306b7603ab1a
GoldyL <vitaliyfnaf@gmail.com>
2025-12-19 20:52:21 +0100

    Update AuthStatus.kt

--- client-core/src/main/java/hivens/core/data/AuthStatus.kt
+++ client-core/src/main/java/hivens/core/data/AuthStatus.kt
@@ -2,19 +2,8 @@ package hivens.core.data
 
 import com.google.gson.annotations.SerializedName
 
-enum class AuthStatus { // TODO: Удалить / использовать неиспользованные статусы
+enum class AuthStatus { // TODO: В будущем расширить
     @SerializedName("OK")
     OK,
-
-    @SerializedName("LOGIN") // Сервер возвращает это, если логин не найден
-    BAD_LOGIN,
-
-    @SerializedName("PASSWORD") // Сервер возвращает это, если пароль неверный
-    BAD_PASSWORD,
-
-    @SerializedName("SERVER") // Сервер возвращает это, если ID сервера неверный
-    SERVER,
-
-    NO_SERVER,
     INTERNAL_ERROR
 }
